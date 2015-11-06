ac50e183c353d6c641df05381d35e4986c31b42c
Jackson Sui <kiro705@gmail.com>
2015-11-06 17:53:27 -0500

    made it so the cards are in a deck that is reshuffled each time, cards have suits

--- BlackJack.js
+++ BlackJack.js
@@ -1,5 +1,6 @@
 //Simple Blackjack game!
 //The chance of any card being drawn is always 1 in 13, even if there have been other cards drawn.
+var cards = [];
 var money = function(){
 	$(document).ready(function(){
 		$('#cash').remove();
@@ -7,6 +8,46 @@ var money = function(){
 		document.getElementById('cash').innerHTML = 'Money: $' + $money;
 	});
 }
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+var makeDeck = function(){
+	var card = function(number, rank, suit){
+		this.number = number;
+		this.rank = rank;
+		this.suit = suit;
+	}
+	var deck = function(){
+		this.ranks = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King'];
+		this.suits = ['Hearts','Diamonds','Spades','Clubs'];
+	
+    
+    	for( var s = 0; s < this.suits.length; s++ ) {
+    	    for( var n = 0; n < this.ranks.length; n++ ) {
+        	    cards.push( new card( n+1, this.ranks[n], this.suits[s] ) );
+        	}
+    	}
+    	shuffle(cards);
+    	return cards;
+	}
+	deck();
+	console.log('the top 4 cards are...');
+	console.log(cards[0]);
+	console.log(cards[1]);
+	console.log(cards[2]);
+	console.log(cards[3]);
+	console.log(cards[4]);
+	console.log(cards[5]);
+}
+
 var $money = 100;
 money();
 var play = function(){
@@ -15,42 +56,38 @@ console.log('Here is $100 to start, and each hand will cost $10.');
 $money = 100;
 
 var playing = true;
-var deck = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King'];
 var value = [11,2,3,4,5,6,7,8,9,10,10,10,10];
 var altValue = [1,2,3,4,5,6,7,8,9,10,10,10,10];
 var hand = [0,0];
 var aces = 0;
 var daces = 0;
 while(playing === true){
-	var counter = 0;
-	var dcounter = 0;
+	makeDeck();
 	var hit = 0;
 	var dealerPlay = false;
 	var resolve = false;
 	console.log ("You have $" + $money + ".");
-	var hand = [Math.floor((Math.random() * 13)),Math.floor((Math.random() * 13))];
-	var draw = [Math.floor((Math.random() * 13)), Math.floor((Math.random() * 13)), Math.floor((Math.random() * 13)), Math.floor((Math.random() * 13))];
-	var dealer = [Math.floor((Math.random() * 13)),Math.floor((Math.random() * 13))];
-	var dnew = [Math.floor((Math.random() * 13)),Math.floor((Math.random() * 13)),Math.floor((Math.random() * 13)),Math.floor((Math.random() * 13))];
+	var hand = [cards.shift(),cards.shift()];
+	var dealer = [cards.shift(),cards.shift()];
 	var score = 0
-	console.log('Your hand is '+ deck[hand[0]] + ' and '+ deck[hand[1]] +".");
-	if (hand[0] === 0 && hand[1] === 0){
+	console.log('Your hand is '+ hand[0].rank + ' and '+ hand[1].rank +".");
+	if (hand[0].rank === 'Ace' && hand[1].rank === 'Ace'){
 		score = 12;
 		console.log('This adds up to 12.');
 		aces = 1;
 	}
-	else if (hand[0] === 0 || hand[1] === 0){
-		score = value[hand[0]] + value[hand[1]];
+	else if (hand[0].rank === 'Ace' || hand[1].rank === 'Ace'){
+		score = value[hand[0].number - 1] + value[hand[1].number - 1];
 		console.log('This adds up to '+ score +'.');
 		aces = 1;
 	}
 	else{
-		score = value[hand[0]] + value[hand[1]];
+		score = value[hand[0].number - 1] + value[hand[1].number - 1];
 		console.log('This adds up to '+ score +'.');
 		aces = 0;
 	}
-	var dscore = value[dealer[0]] + value[dealer[1]];
-	console.log('The dealer has a ' + deck[dealer[0]] + " face up.");
+	var dscore = value[dealer[0].number - 1] + value[dealer[1].number - 1];
+	console.log('The dealer has a ' + dealer[0].rank + " face up.");
 	var deal = true;
 	var stay = false;
 	var hitting = false;
@@ -78,17 +115,17 @@ while(playing === true){
 			dealerPlay = true;
 		}
 		else{
-			var hit = 0;
+			hit = 0;
 			var hitting = true;
 		}
 		while (hitting === true){
-			var hit = window.prompt('Would you like to hit or stay? Type quit to exit game.');
+			hit = window.prompt('Would you like to hit or stay? Type quit to exit game.');
 			if(hit === "hit") {
+				var draw = cards.shift();
 				console.log('You hit!');
-				console.log("You drew a " + deck[draw[counter]]);
+				console.log("You drew a " + draw.rank);
 				if(score>10){
-					score = score + altValue[draw[counter]];
-					counter++;
+					score = score + altValue[draw.number - 1];
 					if (score > 21 && aces === 1){
 						score = score - 10;
 						aces = 0;
@@ -96,11 +133,10 @@ while(playing === true){
 					hitting = false;
 				}
 				else {
-					score = score + value[draw[counter]];
-					if (deck[draw[counter]] === 'Ace'){
+					score = score + value[draw.number - 1];
+					if (draw.rank === 'Ace'){
 						aces = 1;
 					}
-					counter++;
 					hitting = false;
 				}
 				console.log("Your new score is " + score + ".");
@@ -125,33 +161,31 @@ while(playing === true){
 		}		
 	}
 	while (dealerPlay === true){
-		console.log ('The dealer has ' + deck[dealer[0]] + ' and ' + deck[dealer[1]]+ '.');
-		dcounter = 0;
-		if (dealer[0] === 0 && dealer[1] === 0){
+		console.log ('The dealer has ' + dealer[0].rank + ' and ' + dealer[1].rank + '.');
+		if (dealer[0].rank === 'Ace' && dealer[1].rank === 'Ace'){
 			dscore = 12;
 			daces = 1;
 		}
-		else if (dealer[0] === 0 || dealer[1] === 0){
+		else if (dealer[0].rank === 0 || dealer[1].rank === 0){
 			daces = 1;
 		}
 		var dhitting = true;
 		while (dhitting === true){
 			while (dscore < 17){
-				console.log ('The dealer drew ' +deck[dnew[dcounter]]+ '.');
+				var dnew = cards.shift();
+				console.log ('The dealer drew ' + dnew.rank + '.');
 				if(dscore>10){
-					dscore = dscore + altValue[dnew[dcounter]];
+					dscore = dscore + altValue[dnew.number - 1];
 					if (dscore > 21 && daces === 1){
 						dscore = dscore - 10;
 						daces = 0;
 					} 
-					dcounter++;
 				}
 				else {
-					dscore = dscore + value[dnew[dcounter]];
-					if (deck[dnew[dcounter]] === 'Ace'){
+					dscore = dscore + value[dnew.number - 1];
+					if (dnew.rank === 'Ace'){
 						daces = 1;
 					}
-					dcounter++;
 				}
 			}
 			if (dscore > 21) {
