c47a3f2454982f9dd3cd564686757d2918d04b76
Jackson Sui <kiro705@gmail.com>
2015-11-08 16:59:05 -0500

    Version 1.0, finished card images

--- BlackJack.js
+++ BlackJack.js
@@ -14,28 +14,15 @@ var $money = 0;
 money();
 var value = [11,2,3,4,5,6,7,8,9,10,10,10,10];
 var altValue = [1,2,3,4,5,6,7,8,9,10,10,10,10];
+var abbreviations = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
 var score = 0;
 var dscore = 0;
 var dealer = [];
 var hand = [];
 var aces = 0;
 var daces = 0;
-
-var changeText = function(){
-	$(document).ready(function(){
-		var $text1 = text1;
-		var $text2 = text2;
-		var $text3 = text3;
-		$('.text').remove();
-		$('#textBox').prepend("<p class='text' id='text1'></p>");
-		document.getElementById('text1').innerHTML = $text1;
-		$('#textBox').append("<p class='text' id='text2'></p>");
-		document.getElementById('text2').innerHTML = $text2;
-		$('#textBox').append("<p class='text' id='text3'></p>");
-		document.getElementById('text3').innerHTML = $text3;
-	});
-
-};
+var mycard = 0;
+var dcard = 0;
 
 function shuffle(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -55,7 +42,7 @@ var makeDeck = function(){
 	}
 	var deck = function(){
 		this.ranks = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King'];
-		this.suits = ['Hearts','Diamonds','Spades','Clubs'];
+		this.suits = ['heart','diamond','spade','club'];
 	
     
     	for( var s = 0; s < this.suits.length; s++ ) {
@@ -69,6 +56,60 @@ var makeDeck = function(){
 	deck();
 };
 
+var changeText = function(){
+	$(document).ready(function(){
+		var $text1 = text1;
+		var $text2 = text2;
+		var $text3 = text3;
+		$('.text').remove();
+		$('#textBox').prepend("<p class='text' id='text1'></p>");
+		document.getElementById('text1').innerHTML = $text1;
+		$('#textBox').append("<p class='text' id='text2'></p>");
+		document.getElementById('text2').innerHTML = $text2;
+		$('#textBox').append("<p class='text' id='text3'></p>");
+		document.getElementById('text3').innerHTML = $text3;
+	});
+
+};
+
+var showCard = function(){
+	$(document).ready(function(){
+		$('#player').append("<div class='card'>" + abbreviations[mycard.number - 1] + "<div class='img'><img src='img/" + mycard.suit + ".jpg' height='30' width='30'></div></div>")
+	});
+};
+
+var showDcard = function(){
+	$(document).ready(function(){
+		$('#dealer').append("<div class='card'>" + abbreviations[dcard.number - 1] + "<div class='img'><img src='img/" + dcard.suit + ".jpg' height='30' width='30'></div></div>")
+	});
+};
+
+var showBack = function(){
+	$(document).ready(function(){
+		$('#dealer').append("<div class='cardBack'><div class='back'><img src='img/back.jpg' height='95' width='65'></div></div>")
+	});
+};
+
+var clearCards = function(){
+	$(document).ready(function(){
+		$('.card').remove();
+	});
+};
+
+var clearBack = function(){
+	$(document).ready(function(){
+		$('.cardBack').remove();
+	});
+};
+
+var letsplay = function(){
+	$(document).ready(function(){
+		$('#play').remove();
+		$('#buttonBox').append("<button class='button' id='deal' onclick=dealme()>Deal</button>");
+	});
+	play();
+};
+
 var play = function(){
 	text1 = 'Welcome to the Blackjack table!';
 	text2 = 'Here is $100 to start, and each hand will cost $10.';
@@ -80,16 +121,81 @@ var play = function(){
 	daces = 0;
 };
 
-var letsplay = function(){
-	$(document).ready(function(){
-		$('#play').remove();
-		$('#buttonBox').append("<button class='button' id='deal' onclick=dealme()>Deal</button>");
-	});
-	play();
+var dealme = function(){
+	clearCards();
+	if ($money < 10){
+		$(document).ready(function(){
+			$('.button').remove();
+			text1 = "You don't have enough money to play!";
+			text2 = "You lost.";
+			text3 = '';
+			changeText();
+		});
+	}
+	else{
+		$(document).ready(function(){
+			$('.button').remove();
+		});
+		$money = $money - 10;
+		money();
+		dealPhase();
+	}
+};
+
+var dealPhase = function() {
+	cards = [];
+	makeDeck();
+	hand = [];
+	dealer = [];
+
+	//Player's 1st card
+	mycard = cards.shift();
+	showCard();
+	hand.push(mycard);
+
+	//Dealer's 1st card
+	dcard = cards.shift();
+	showDcard();
+	dealer.push(dcard);
+
+	//Player's 2nd card
+	mycard = cards.shift();
+	showCard();
+	hand.push(mycard);
+
+	//Dealer's 2nd card (facedown)
+	dcard = cards.shift();
+	showBack();
+	dealer.push(dcard);
+
+	text1 = 'Your hand is '+ hand[0].rank + ' and '+ hand[1].rank +".";
+	console.log('Your hand is '+ hand[0].rank + ' and '+ hand[1].rank +".");
+	if (hand[0].rank === 'Ace' && hand[1].rank === 'Ace'){
+		score = 12;
+		text2 = 'This adds up to 12.';
+		aces = 1;
+	}
+	else if (hand[0].rank === 'Ace' || hand[1].rank === 'Ace'){
+		score = value[hand[0].number - 1] + value[hand[1].number - 1];
+		text2 = 'This adds up to '+ score +'.';
+		aces = 1;
+	}
+	else{
+		score = value[hand[0].number - 1] + value[hand[1].number - 1];
+		text2 = 'This adds up to '+ score +'.';
+		aces = 0;
+	}
+	dscore = value[dealer[0].number - 1] + value[dealer[1].number - 1];
+	console.log('The dealer has ' + dealer[0].rank + " and " + dealer[1].rank + " face up.");
+	text3 = 'The dealer has a ' + dealer[0].rank + " face up.";
+	changeText();
+	hitButtons();
 };
 
 var ihit = function(){
 	var draw = cards.shift();
+	mycard = draw;
+	showCard();
 	text1 = 'You have hit!';
 	text2 = 'You drew a ' + draw.rank;
 	if(score>10){
@@ -124,57 +230,6 @@ var westay = function(){
 	resolveButton();
 };
 
-
-var dealPhase = function() {
-	cards = [];
-	makeDeck();
-	hand = [cards.shift(),cards.shift()];
-	dealer = [cards.shift(),cards.shift()];
-	score = 0
-	text1 = 'Your hand is '+ hand[0].rank + ' and '+ hand[1].rank +".";
-	console.log('Your hand is '+ hand[0].rank + ' and '+ hand[1].rank +".");
-	if (hand[0].rank === 'Ace' && hand[1].rank === 'Ace'){
-		score = 12;
-		text2 = 'This adds up to 12.';
-		aces = 1;
-	}
-	else if (hand[0].rank === 'Ace' || hand[1].rank === 'Ace'){
-		score = value[hand[0].number - 1] + value[hand[1].number - 1];
-		text2 = 'This adds up to '+ score +'.';
-		aces = 1;
-	}
-	else{
-		score = value[hand[0].number - 1] + value[hand[1].number - 1];
-		text2 = 'This adds up to '+ score +'.';
-		aces = 0;
-	}
-	dscore = value[dealer[0].number - 1] + value[dealer[1].number - 1];
-	console.log('The dealer has ' + dealer[0].rank + " and " + dealer[1].rank + " face up.");
-	text3 = 'The dealer has a ' + dealer[0].rank + " face up.";
-	changeText();
-	hitButtons();
-};
-
-var dealme = function(){
-	if ($money < 10){
-		$(document).ready(function(){
-			$('.button').remove();
-			text1 = "You don't have enough money to play!";
-			text2 = "You lost.";
-			text3 = '';
-			changeText();
-		});
-	}
-	else{
-		$(document).ready(function(){
-			$('.button').remove();
-		});
-		$money = $money - 10;
-		money();
-		dealPhase();
-	}
-};
-
 var hitButtons = function(){
 	$(document).ready(function(){
 		$('.button').remove();
@@ -199,6 +254,7 @@ var newDeal = function(){
 };
 
 var walk = function(){
+	clearCards();
 	$(document).ready(function(){
 		$('.button').remove();
 	});
@@ -217,6 +273,8 @@ var walk = function(){
 };
 
 var resolvePhase = function(){
+	clearBack();
+	showDcard();
 	text1 = 'The dealer has ' + dealer[0].rank + ' and ' + dealer[1].rank + '.';
 	if (dealer[0].rank === 'Ace' && dealer[1].rank === 'Ace'){
 		dscore = 12;
@@ -229,6 +287,8 @@ var resolvePhase = function(){
 	while (dhitting === true){
 		while (dscore < 17){
 			var dnew = cards.shift();
+			dcard = dnew;
+			showDcard();
 			console.log ('The dealer drew ' + dnew.rank + '.');
 			if(dscore>10){
 				dscore = dscore + altValue[dnew.number - 1];
@@ -245,7 +305,7 @@ var resolvePhase = function(){
 			}
 		}
 		if (dscore > 21) {
-			dscore = 'busted!';
+			dscore = 'busted';
 			dhitting = false;
 		}
 		else {
